b4999ab2349f79efbbaca7154d6c0a407e7a7247
DiadjiBadiane <diadjibadiane77@gmail.com>
2025-01-09 10:40:07 +0100

    exercice avec tests validés

--- funcs.py
+++ funcs.py
@@ -2,4 +2,8 @@
 
 #fonction qui renvoie les 3 plus grandes valeurs d' une liste d' entier
 def max_listes(liste_entiers):
-    pass
+    nouvelle_liste = sorted(liste_entiers)
+    return(nouvelle_liste[len(nouvelle_liste) - 3:len(nouvelle_liste)])
+
+if __name__ == '__main__':
+    max_listes([1, 2, 3, 4, 5, 6])
